892d3960c1ad38d612e0c0f2114e88a961588ea5
OliBomby <olivierschipper@gmail.com>
2023-11-19 01:55:30 +0100

    rename vars

--- bspline.py
+++ bspline.py
@@ -13,19 +13,19 @@ def bspline_basis(p, n, x):
     Returns:
         array, matrix array of B-spline basis function values at x
     """
-    t = np.pad(np.linspace(0, 1, n + 1 - p), (p, 2), constant_values=(0, 1))  # knot vector
-    basi = np.empty((p + 1, len(x), n + p))
+    u = np.pad(np.linspace(0, 1, n + 1 - p), (p, 2), constant_values=(0, 1))  # knot vector
+    basis = np.empty((p + 1, len(x), n + p))
     xb = x[:, None]
 
     for c in range(p + 1):
         if c == 0:
-            basi[c] = np.where(xb != 1, np.where((t[None, :-1] <= xb) & (xb < t[None, 1:]), 1, 0), np.where(t[1:] == 1, 1, 0)[None, :])
+            basis[c] = np.where(xb != 1, np.where((u[None, :-1] <= xb) & (xb < u[None, 1:]), 1, 0), np.where(u[1:] == 1, 1, 0)[None, :])
         else:
-            alpha = np.nan_to_num((xb - t[None, :n + p - c]) / (t[c:n + p] - t[:n + p - c])[None, :])
-            beta = np.nan_to_num((t[None, c + 1:n + p + 1] - xb) / (t[c + 1:n + p + 1] - t[1:n + p - c + 1])[None, :])
-            basi[c, :, :n + p - c] = alpha * basi[c - 1, :, :n + p - c] + beta * basi[c - 1, :, 1:n + p - c + 1]
+            alpha = np.nan_to_num((xb - u[None, :n + p - c]) / (u[c:n + p] - u[:n + p - c])[None, :])
+            beta = np.nan_to_num((u[None, c + 1:n + p + 1] - xb) / (u[c + 1:n + p + 1] - u[1:n + p - c + 1])[None, :])
+            basis[c, :, :n + p - c] = alpha * basis[c - 1, :, :n + p - c] + beta * basis[c - 1, :, 1:n + p - c + 1]
 
-    return basi[-1, :, :n]
+    return basis[-1, :, :n]
 
 
 if __name__ == "__main__":
